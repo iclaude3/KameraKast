f523517b39521347f578299b484133528fa449cd
iclaude3 <cmbandali3@gmail.com>
2019-06-19 14:08:37 -0400

    Added Bottom Navigation

--- app/src/main/java/com/io/kamerakast/MainActivity.java
+++ app/src/main/java/com/io/kamerakast/MainActivity.java
@@ -1,13 +1,41 @@
 package com.io.kamerakast;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_stream:
+                        Toast.makeText(MainActivity.this, "Stream",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_connect:
+                        Toast.makeText(MainActivity.this, "Connect",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_home:
+                    default:
+                        Toast.makeText(MainActivity.this, "Home",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
